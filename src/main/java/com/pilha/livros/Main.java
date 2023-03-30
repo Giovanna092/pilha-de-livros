4267413ed8fdcac3b93cbc873807ac703894cb45
Giovanna Costa <giovannadecosta@gmail.com>
2023-03-30 14:18:58 -0300

    Refatorando switch case

--- src/main/java/com/pilha/livros/Main.java
+++ src/main/java/com/pilha/livros/Main.java
@@ -1,5 +1,6 @@
 package com.pilha.livros;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -9,44 +10,36 @@ public class Main {
         Stack<String> pilhaLivros = new Stack<>();
 
 
-        String tituloLivro = "Titulo do livro";
+        String tituloLivro;
         int opcoes=0;
-        while (!(opcoes==5)) {
+        while (!(opcoes == 5)) {
             if (pilhaLivros.isEmpty()) {
                 System.out.println("A pilha está vazia. Adicione um novo livro");
                 System.out.println("Titulo do livro que será adicionado: ");
                 tituloLivro = entrada.nextLine();
                 pilhaLivros.push(tituloLivro);
-                System.out.println("Livro '"+ tituloLivro +"' adicionado");
+                System.out.println("Livro '" + tituloLivro + "' adicionado");
             }
 
             System.out.println("Opcoes:\n[1] adicionar novo livro\n[2] remover livro\n[3] mostrar livro no topo\n[4] mostrar pilha de livros\n[5] sair");
-            opcoes = op.nextInt();
-            switch (opcoes){
-                case 1:
+
+                opcoes = op.nextInt();
+            switch (opcoes) {
+                case 1 -> {
                     System.out.println("Titulo do livro que será adicionado: ");
                     tituloLivro = entrada.nextLine();
                     pilhaLivros.push(tituloLivro);
-                    System.out.println("Livro '"+ tituloLivro +"' adicionado");
-                    break;
-                case 2:
-                    System.out.println("Livro '"+pilhaLivros.pop().getTitle()+"' removido");
-
-                    break;
-                case 3:
-                    System.out.println("Livro '"+pilhaLivros.top().getTitle()+"'");
-                    break;
-                case 4:
-                    System.out.println(pilhaLivros);
-                    break;
-                case 5:
-                    System.out.println("Fim da execucão.");
-                    break;
-                default:
-                    System.out.println("valor invalido. Tente novamente!");
-                    break;
+                    System.out.println("Livro '" + tituloLivro + "' adicionado");
+                }
+                case 2 -> System.out.println("Livro '" + pilhaLivros.pop().getTitle() + "' removido");
+                case 3 -> System.out.println("Livro '" + pilhaLivros.top().getTitle() + "'");
+                case 4 -> System.out.println(pilhaLivros);
+                case 5 -> System.out.println("Fim da execucão.");
+                default -> System.out.println("valor invalido. Tente novamente!");
             }
+
         }
-        entrada.close();
+
+
     }
 }
